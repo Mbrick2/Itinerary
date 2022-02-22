b502719fc4859f2f3182d09b4d30210739fe394d
Mbrick2 <90691415+Mbrick2@users.noreply.github.com>
2022-02-22 20:47:29 +1100

    Create developer.vue

---
+++ pages/developer.vue
@@ -0,0 +1,24 @@
+
+<template>
+    <div class="container">
+        <Navigation />
+        <div class="content">
+            <div class="card">
+                <h1>Contributing</h1>
+                <p></p>            </div>
+        </div>
+        <Footer />
+    </div>
+</template>
+
+<script>
+    export default {
+        head() {
+            return {
+                title: 'Developers | Itinerary'
+            };
+        }
+    };
+</script>
+
+<style scoped
